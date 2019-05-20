1b43af832ac7224f790b3e95763cd0728df5be6c
patspeiser <pat@patspeiser.com>
2019-05-20 02:15:54 -0400

    debugging adding to net

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 from PIL import Image
 import datetime
 BATCH_SIZE = 32
-DEV_IMG_COUNT = 3000
+DEV_IMG_COUNT = 10
 ssl._create_default_https_context = ssl._create_unverified_context
 tf.enable_eager_execution()
 AUTOTUNE = tf.data.experimental.AUTOTUNE
@@ -36,28 +36,49 @@ def get_all_images(path_to_images):
 
 def preprocess_image(image):
 	print('...preprocessing')
-	img = tf.image.decode_png(image)
-	img_final = tf.image.resize(img, [192, 192])		
+	image_raw = tf.io.read_file(image)
+	img = tf.image.decode_png(image_raw)
+	img_final = tf.image.resize(img, [224, 224])		
 	#normalize 
 	img_final /= 255.0
-	print(img_final)
 	return img_final
 
 def create_tensors(images):
 	print('...creating tensors')
 	print('Total number of images: ', len(images))
+	processed = list()
 	#create tensors
 	counter = 0
 	for image in images:
-		if(counter <= 10):
-			print(counter)
-			try:
-				preprocess_image(image)
-			except:
-				print('exception', image)	
+		try:
+			processed.append(preprocess_image(image))
 			counter+=1
+		except:
+			print('exception')	
 	print('Successfully Processed: ', counter)
-	return images
+	return processed
+
+
+def build_tensorflow_dataset(images):
+	print('...building tensorflow dataset from', len(images), 'images')
+		
+	#slice array of paths into dataset of paths
+	path_tensors = create_tensors(images)
+	image_dataset = tf.data.Dataset.from_tensor_slices(path_tensors)
+
+	#need a label for each item
+	labels = list()
+	for i in range(len(path_tensors)):
+		labels.append('bone_human')
+
+	label_dataset = tf.data.Dataset.from_tensor_slices(labels)
+	print('...labels created:', len(labels))
+
+	print('...creating dataset')
+	data = (image_dataset, label_dataset)
+	dataset = tf.data.Dataset.zip(data) 
+	print('...returning dataset')
+	return dataset
 
 def create_labels(count):
 	print('...creating labels')
@@ -68,62 +89,43 @@ def create_labels(count):
 		i+=1
 	print(len(labels), ' labels created')
 	return labels
-
-def build_tensorflow_dataset(images):
-	print('...building tensorflow dataset from', len(images), 'images')
-		
-	#slice array of paths into dataset of paths
-	path_dataset = tf.data.Dataset.from_tensor_slices(images)
-
-	#preprocess all images
-	image_dataset = path_dataset.map(preprocess_image, num_parallel_calls=BATCH_SIZE)
-	print('...images created', image_dataset)
-	
-	#need a label for each item
-	labels = list()
-	for i in range(len(images)):
-		labels.append('bone_human')
-	label_dataset = tf.data.Dataset.from_tensor_slices(labels)
-	print('...labels created', label_dataset)
-
-	print('...creating dataset')
-	data = (image_dataset, label_dataset)
-	dataset = tf.data.Dataset.zip(data) 
-	print('...returning dataset', dataset)
 	return dataset
 
 def get_keras_dataset(dataset, buffer_size):
 	print('...getting keras dataset', dataset)
+	dataset.map(change_range)
 	data = dataset.shuffle(buffer_size=buffer_size) 
 	data = data.repeat()
 	data = data.batch(BATCH_SIZE)
 	data = data.prefetch(buffer_size=buffer_size)
-	keras_dataset = data.map(change_range) 
-	return keras_dataset
+	print(data)
+	return data
 
-def change_range(image,label):
-	return 2*image-1, label
+def change_range(image, label):
+	ranged_image = 2*image-1
+	return ranged_image, label
 
 def run_training(training_csv):
 	start = datetime.datetime.now()
 	print('Starting: ', start)
-	
-	#get image paths from model csv
 	images = get_all_images(training_csv)
-	#create a dataset from processed images
 	dataset = build_tensorflow_dataset(images)
-	#get a keras dataset
 	keras_dataset = get_keras_dataset(dataset, len(images))
-
-	mobile_net = tf.keras.applications.MobileNetV2(input_shape=(192, 192, 3), include_top=False)
+	mobile_net = tf.keras.applications.MobileNetV2(include_top=True, alpha=1.0)
 	mobile_net.trainable = False
-	image_batch, label_batch = next(iter(keras_dataset))
-	feature_map_batch = mobile_net(image_batch)
-	print(feature_map_batch.shape)
-	#print(feature_map_batch.shape)
-
-	model = tf.keras.Sequential()
-	model.add(feature_map_batch)
+	for i in keras_dataset:
+		print(i[0], i[1])
+		#mobile_net(i)
+	#batch = mobile_net(image_batch)
+	#print(batch.shape)
+	"""	
+	model = tf.keras.Sequential([
+		mobile_net,
+		tf.keras.layers.GlobalAveragePooling2D(),
+		tf.keras.layers.Dense(len(images))
+	])
+	"""
+	#model.add(image_batch)
 	end   = datetime.datetime.now()
 	print ('Ending: ', end)
 	print('Total Run Time', end - start)
