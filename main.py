05dca01762f467988b6e66ace212021cd108fd69
patspeiser <pat@patspeiser.com>
2019-05-20 19:24:12 -0400

    model building

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import csv
 import random
 from PIL import Image
 import datetime
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 BATCH_SIZE = 32
 DEV_IMG_COUNT = 10
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -19,10 +21,8 @@ training_csv	= training_model + '/train_image_paths.csv'
 
 #gets list of images
 def get_all_images(path_to_images):
-	print('...getting all images')
 	counter=0
 	list_of_images  = list()
-	print('...opening training file csv')
 	with open(path_to_images, 'rt') as training_file: 
 		reader = csv.DictReader(training_file)
 		for row in reader:
@@ -35,16 +35,15 @@ def get_all_images(path_to_images):
 	return list_of_images
 
 def preprocess_image(image):
-	print('...preprocessing')
 	image_raw = tf.io.read_file(image)
-	img = tf.image.decode_png(image_raw)
-	img_final = tf.image.resize(img, [224, 224])		
+	img_tensor = tf.image.decode_png(image_raw, channels=3)
+	img_final = tf.image.resize(img_tensor, [224, 224])		
 	#normalize 
 	img_final /= 255.0
+	#print(img_final.shape, img_final.numpy().min(), img_final.numpy().max())
 	return img_final
 
 def create_tensors(images):
-	print('...creating tensors')
 	print('Total number of images: ', len(images))
 	processed = list()
 	#create tensors
@@ -53,8 +52,8 @@ def create_tensors(images):
 		try:
 			processed.append(preprocess_image(image))
 			counter+=1
-		except:
-			print('exception')	
+		except Exception as e:
+			print(e)	
 	print('Successfully Processed: ', counter)
 	return processed
 
@@ -63,21 +62,17 @@ def build_tensorflow_dataset(images):
 	print('...building tensorflow dataset from', len(images), 'images')
 		
 	#slice array of paths into dataset of paths
-	path_tensors = create_tensors(images)
-	image_dataset = tf.data.Dataset.from_tensor_slices(path_tensors)
+	tensors = create_tensors(images)
+	image_dataset = tf.data.Dataset.from_tensor_slices(tensors)
 
 	#need a label for each item
 	labels = list()
-	for i in range(len(path_tensors)):
+	for i in range(len(tensors)):
 		labels.append('bone_human')
 
-	label_dataset = tf.data.Dataset.from_tensor_slices(labels)
-	print('...labels created:', len(labels))
-
-	print('...creating dataset')
-	data = (image_dataset, label_dataset)
-	dataset = tf.data.Dataset.zip(data) 
-	print('...returning dataset')
+	#label_dataset = tf.data.Dataset.from_tensor_slices(labels)
+	#data = (image_dataset, label_dataset)
+	dataset = tf.data.Dataset.zip(image_dataset) 
 	return dataset
 
 def create_labels(count):
@@ -92,8 +87,8 @@ def create_labels(count):
 	return dataset
 
 def get_keras_dataset(dataset, buffer_size):
-	print('...getting keras dataset', dataset)
-	dataset.map(change_range)
+	for d in dataset:
+		change_range(d)
 	data = dataset.shuffle(buffer_size=buffer_size) 
 	data = data.repeat()
 	data = data.batch(BATCH_SIZE)
@@ -101,9 +96,9 @@ def get_keras_dataset(dataset, buffer_size):
 	print(data)
 	return data
 
-def change_range(image, label):
-	ranged_image = 2*image-1
-	return ranged_image, label
+def change_range(image):
+	in_range_image = 2*image-1
+	return in_range_image
 
 def run_training(training_csv):
 	start = datetime.datetime.now()
@@ -111,21 +106,24 @@ def run_training(training_csv):
 	images = get_all_images(training_csv)
 	dataset = build_tensorflow_dataset(images)
 	keras_dataset = get_keras_dataset(dataset, len(images))
-	mobile_net = tf.keras.applications.MobileNetV2(include_top=True, alpha=1.0)
+	mobile_net = tf.keras.applications.MobileNetV2(input_shape=(224,224,3), include_top=True, alpha=1.0)
 	mobile_net.trainable = False
 	for i in keras_dataset:
-		print(i[0], i[1])
-		#mobile_net(i)
-	#batch = mobile_net(image_batch)
-	#print(batch.shape)
-	"""	
+		print('adding to model', datetime.datetime.now())
+		mobile_net(i)
+
 	model = tf.keras.Sequential([
 		mobile_net,
 		tf.keras.layers.GlobalAveragePooling2D(),
 		tf.keras.layers.Dense(len(images))
 	])
-	"""
-	#model.add(image_batch)
+	
+	model.compile(optimizer=tf.train.AdamOptimizer(), 
+		loss=tf.keras.losses.sparse_categorical_crossentropy,
+		metrics=["accuracy"])
+
+	model.summary()
+
 	end   = datetime.datetime.now()
 	print ('Ending: ', end)
 	print('Total Run Time', end - start)
